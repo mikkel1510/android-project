26e69819497cc1b6cce46b869f7627f7c4e53aaf
Nikolai Bruun <nibru23@student.sdu.dk>
2025-11-13 10:48:26 +0100

    Added groupViewModel and first draft of group creation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
