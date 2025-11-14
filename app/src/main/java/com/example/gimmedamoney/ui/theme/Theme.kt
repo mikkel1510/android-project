516268a904cd59b898e903625df3dbd366a6a918
Mikkel Nielsen <mikkel1510@live.dk>
2025-11-14 23:48:16 +0100

    Defined custom color palette

--- app/src/main/java/com/example/gimmedamoney/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gimmedamoney/ui/theme/Theme.kt
@@ -9,34 +9,40 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BrandBlue,
+    onPrimary = Color.White,
+
+    background = DarkGreyBackground,
+    onBackground = Color.White,
+
+    surface = DarkGreySurface,
+    onSurface = Color.White,
+
+    secondary = BrandBlue,
+    tertiary = BrandBlue
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = BrandBlue,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+
+    background = Color.White,
+    onBackground = Color(0xFF111827),
+
+    surface = LightGreyBackground,
+    onSurface = Color(0xFF111827),
+
+    secondary = BrandBlue,
+    tertiary = BrandBlue
 )
 
 @Composable
 fun GimmeDaMoneyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
